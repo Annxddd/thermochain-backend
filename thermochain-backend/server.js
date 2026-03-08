f53b7dc7e95a8531a1c6574b5c82666d7ae7ef1c
Annxddd <avallarinorodriguez@gmail.com>
2026-03-08 12:56:08 -0500

    Update SMTP credentials in server.js
    Changed SMTP user and password to hardcoded values.

--- thermochain-backend/server.js
+++ thermochain-backend/server.js
@@ -56,8 +56,8 @@ const mailer = nodemailer.createTransport({
   port  : 587,
   secure: false,
   auth  : {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS   // Google App Password
+    user: 'thermochainsolutions@gmail.com',
+    pass: 'irrw pwav yesp qmcx'   // Google App Password
   }
 });
 
